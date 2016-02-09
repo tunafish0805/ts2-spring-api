2556a69b97f94380eff6e0c3d6ffd88a36675137
carofr01 <carofr01@USLJ7VMP32.enterprisenet.org>
2016-02-09 18:03:46 -0500

    added comments

--- SpringTestApp/src/com/springTestApp/controller/TestController.java
+++ SpringTestApp/src/com/springTestApp/controller/TestController.java
@@ -35,7 +35,7 @@ public class TestController {
     @Autowired
     private TestService testService;
     
-    // comment
+    // comment 
     @RequestMapping(value = "/data", method = RequestMethod.GET)
     public String get(@RequestParam("importIndex") String importIndex, @RequestParam("marketIndex") String marketIndex,
                     @RequestParam("timeIndex") String timeIndex, @RequestParam("measureIndex") String measureIndex) {
