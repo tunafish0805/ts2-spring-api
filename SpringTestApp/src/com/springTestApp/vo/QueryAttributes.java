4f21dd4cd380485676c8d8f1e68071104538e5a6
carofr01 <carofr01@USLJ7VMP32.enterprisenet.org>
2016-02-10 10:14:15 -0500

    formatting yo

--- SpringTestApp/src/com/springTestApp/vo/QueryAttributes.java
+++ SpringTestApp/src/com/springTestApp/vo/QueryAttributes.java
@@ -5,30 +5,37 @@ public class QueryAttributes {
     String marketIndex;
     String timeIndex;
     String measureIndex;
+
     public String getImportIndex() {
         return importIndex;
     }
+
     public void setImportIndex(String importIndex) {
         this.importIndex = importIndex;
     }
+
     public String getMarketIndex() {
         return marketIndex;
     }
+
     public void setMarketIndex(String marketIndex) {
         this.marketIndex = marketIndex;
     }
+
     public String getTimeIndex() {
         return timeIndex;
     }
+
     public void setTimeIndex(String timeIndex) {
         this.timeIndex = timeIndex;
     }
+
     public String getMeasureIndex() {
         return measureIndex;
     }
+
     public void setMeasureIndex(String measureIndex) {
         this.measureIndex = measureIndex;
     }
-    
-    
+
 }
